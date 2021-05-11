f19cb6c09a0e4a09895700216b2acb22dd96ac05
moooosh <73801599+moooosh@users.noreply.github.com>
2021-05-11 15:07:45 +1000

    Removed React Logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className = "container">
+
       <header className="App-header">
         <img src="./dictionarylogo.png" className="App-logo" alt="logo" />
        
       </header>
-    </div></div>
+    </div>
   );
 }
 
