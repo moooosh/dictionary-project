4ee88d53f133a63328880104fd6b32994c561b02
moooosh <73801599+moooosh@users.noreply.github.com>
2021-05-11 14:58:52 +1000

    Added Logo & CSS

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className = "container">
       <header className="App-header">
-
+        <img src="./dictionarylogo.png" className="App-logo" alt="logo" />
        
       </header>
-    </div>
+    </div></div>
   );
 }
 
